0cfaed7c512a2c43e235c3cd687d5d4c9a2764f7
vilunarka <bunina.vika876@gmail.com>
2024-10-25 12:55:24 +0300

    Update task1.py

--- Лабораторная работа 1/task1.py
+++ Лабораторная работа 1/task1.py
@@ -1,4 +1,4 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-mean = ( sum(numbers[:4]) + sum(numbers[5:]) ) / len(numbers)
+mean = (sum(numbers[:4]) + sum(numbers[5:])) / len(numbers)
 numbers[4] = mean
 print("Измененный список:", numbers)
