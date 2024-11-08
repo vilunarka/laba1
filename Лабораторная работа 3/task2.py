32f7cf70097eda21f16b1151746ba55308565a21
vilunarka <bunina.vika876@gmail.com>
2024-11-08 09:06:51 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,10 +1,9 @@
-def find_common_participants(a, b, c=","):
+def find_common_participants(first_group, second_group, separator=","):
     out = []
-    a = a.split(c)
-    b = b.split(c)
-
-    for i in a:
-        for j in b:
+    first_group = first_group.split(separator)
+    second_group = second_group.split(separator)
+    for i in first_group:
+        for j in second_group:
             if i == j:
                 out.append(i)
 
@@ -13,7 +12,5 @@ def find_common_participants(a, b, c=","):
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-
-# TODO Провеьте работу функции с разделителем отличным от запятой
 
 print(find_common_participants(participants_first_group, participants_second_group, "|"))
