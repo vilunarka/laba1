26e5e877441bdfb2560700e175d6ae1fff507fc5
vilunarka <bunina.vika876@gmail.com>
2024-11-08 20:35:05 +0300

    Update task2.py

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -2,14 +2,13 @@ def find_common_participants(first_group, second_group, separator=","):
     out = []
     first_group = first_group.split(separator)
     second_group = second_group.split(separator)
-    for i in first_group:
-        for j in second_group:
-            if i == j:
-                out.append(i)
-
+    for index1 in first_group:
+        for index2 in second_group:
+            if index1 == index2:
+                out.append(index1)
     out.sort()
     return(out)
-
+    
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
