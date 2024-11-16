802de5b9fb7bdbfd292975813527912f22be4fe9
vilunarka <bunina.vika876@gmail.com>
2024-11-16 19:28:48 +0300

    Update task2.py

--- Лабораторная работа 4/task2.py
+++ Лабораторная работа 4/task2.py
@@ -1,17 +1,13 @@
 import csv
 import json
-
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
-
 def task() -> None:
     with open(INPUT_FILENAME, 'r') as input_file:
         csv_file = csv.DictReader(input_file)
         rows = list(csv_file)
-
     with open(OUTPUT_FILENAME, 'w') as output:
         output.write(json.dumps(rows, indent=4))
-
 if __name__ == '__main__':
     # Нужно для проверки
     task()
