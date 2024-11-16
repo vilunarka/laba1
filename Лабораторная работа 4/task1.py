25b511808f7041f718938d9757fe7d7352beeb3f
vilunarka <bunina.vika876@gmail.com>
2024-11-16 19:28:31 +0300

    Update task1.py

--- Лабораторная работа 4/task1.py
+++ Лабораторная работа 4/task1.py
@@ -1,12 +1,10 @@
 import json
 file_input = "input.json"
-
 def task() -> float:
     with open(file_input, 'r') as file:
         json_file = json.load(file)
         multiplication = [item["weight"] * item["score"] for item in json_file]
         result = sum(multiplication)
     return round(result, 3)
-
 if __name__ == '__main__':
     print(task())
