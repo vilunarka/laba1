dbf216288e0fd5d58957bbdbf3b0726b9227d645
vilunarka <bunina.vika876@gmail.com>
2024-11-16 19:54:31 +0300

    Update task1.py

--- Лабораторная работа 4/task1.py
+++ Лабораторная работа 4/task1.py
@@ -1,21 +1,15 @@
-import csv
 import json
 
-INPUT_FILENAME = "input.csv"
-OUTPUT_FILENAME = "output.json"
+file_input = "input.json"
 
 
-def task() -> None:
-    with open(INPUT_FILENAME, 'r') as input_file:
-        csv_file = csv.DictReader(input_file)
-        rows = list(csv_file)
-    with open(OUTPUT_FILENAME, 'w') as output:
-        output.write(json.dumps(rows, indent=4))
+def task() -> float:
+    with open(file_input, 'r') as file:
+        json_file = json.load(file)
+        multiplication = [item["weight"] * item["score"] for item in json_file]
+        result = sum(multiplication)
+    return round(result, 3)
 
 
 if __name__ == '__main__':
-    # Нужно для проверки
-    task()
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
-            print(line, end="")
+    print(task())
